79380fee985bbad555c809232fa45d1fa0f646b9
iridwant <iftikarridwan@gmail.com>
2020-12-11 09:22:55 +0700

    Undo splitting

--- src/views/Rooms.vue
+++ src/views/Rooms.vue
@@ -7,94 +7,114 @@
           <h1 class="text-muted waiting-room-title">Waiting Room...</h1>
         </div>
       </div>
-      <PlayerList></PlayerList>
+      <div class="col-7 d-flex flex-column justify-content-center">
+        <div class="card rooms-card">
+          <div class="card-body d-flex flex-column justify-content-center">
+            <div class="align-self-center">
+              <h1>Players</h1>
+              <div class="player-container scrollable d-flex flex-wrap">
+                <div class="card player-card">
+                  <div class="card-body d-flex flex-column justify-content-center">
+                    <div class="align-self-center">
+                      <h5 class="username-card">Wendy</h5>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <button class="play-btn">Play</button>
+              <h5 class="waiting-info text-muted">Current game is running, please wait</h5>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import PlayerList from '../components/PlayerList.vue'
+// import PlayerList from '../components/PlayerList.vue'
 export default {
   components: {
-    PlayerList
+    // PlayerList
   },
   name: 'Rooms',
   data () {
-    return {
-    }
+    return {}
   }
 }
+
 </script>
 
 <style>
-.row {
-  background-color: #10ac84;
-}
+  .row {
+    background-color: #10ac84;
+  }
 
-.waiting-illustration {
-  width: 30vw;
-}
+  .waiting-illustration {
+    width: 30vw;
+  }
 
-.illustration-side {
-  height: 100vh;
-  width: 100vw;
-}
+  .illustration-side {
+    height: 100vh;
+    width: 100vw;
+  }
 
-.waiting-room-title {
-  margin-top: 16px;
-}
+  .waiting-room-title {
+    margin-top: 16px;
+  }
 
-.rooms-card {
-  width: 50vw;
-  height: 60vh;
-  box-shadow: 0px 3px 15px rgba(0,0,0,0.4);
-  border-radius: 25px !important;
-}
+  .rooms-card {
+    width: 50vw;
+    height: 60vh;
+    box-shadow: 0px 3px 15px rgba(0, 0, 0, 0.4);
+    border-radius: 25px !important;
+  }
 
-.player-container {
-  margin-top: 16px;
-  width: 43vw;
-  height: 35vh;
-  max-width: 43vw;
-  max-height: 35vh;
-  overflow-x: auto;
-}
+  .player-container {
+    margin-top: 16px;
+    width: 43vw;
+    height: 35vh;
+    max-width: 43vw;
+    max-height: 35vh;
+    overflow-x: auto;
+  }
 
-.player-card {
-  width: 8vw;
-  height: 12vh;
-  box-shadow: 0px 3px 5px rgba(0,0,0,0.1);
-  border-radius: 5px !important;
-  margin-top: 8px;
-  margin-left: 8px;
-}
+  .player-card {
+    width: 8vw;
+    height: 12vh;
+    box-shadow: 0px 3px 5px rgba(0, 0, 0, 0.1);
+    border-radius: 5px !important;
+    margin-top: 8px;
+    margin-left: 8px;
+  }
 
-.user-avatar {
-  width: 4vw;
-}
+  .user-avatar {
+    width: 4vw;
+  }
 
-.username-card {
-  margin-top: 4px;
-}
+  .username-card {
+    margin-top: 4px;
+  }
 
-.play-btn {
-  margin-top: 16px;
-  background-color: #a1be02;
-  color: #ffffff;
-  font-size: 25px;
-  border-radius: 10px;
-  padding: 12px;
-  border: 0;
-  box-shadow: 0px 3px 15px rgba(0,0,0,0.3);
-  width: 10vw;
-}
+  .play-btn {
+    margin-top: 16px;
+    background-color: #a1be02;
+    color: #ffffff;
+    font-size: 25px;
+    border-radius: 10px;
+    padding: 12px;
+    border: 0;
+    box-shadow: 0px 3px 15px rgba(0, 0, 0, 0.3);
+    width: 10vw;
+  }
 
-.play-btn:hover {
-  transform: scale(1.05);
-  cursor: pointer;
-}
+  .play-btn:hover {
+    transform: scale(1.05);
+    cursor: pointer;
+  }
 
-.waiting-info {
-  margin-top: 24px;
-}
+  .waiting-info {
+    margin-top: 24px;
+  }
+
 </style>
